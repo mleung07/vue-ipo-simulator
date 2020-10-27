d3b5ec1ecd23e4d30cad2fe589b982fa4a4f3e16
mleung07 <mleung_07@hotmail.com>
2020-10-27 22:40:13 +0800

    Updates

---
+++ precache-manifest.aa99049bee6a8f5d7fb6fc4f061fbdf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c958b6f32d60c10a184",
+    "url": "/vue-ipo-simulator/css/app.cdaa7bca.css"
+  },
+  {
+    "revision": "ab4969135f519a8c9162ab0c38a829bc",
+    "url": "/vue-ipo-simulator/index.html"
+  },
+  {
+    "revision": "0c958b6f32d60c10a184",
+    "url": "/vue-ipo-simulator/js/app.2c41a2c7.js"
+  },
+  {
+    "revision": "dda5e98e02d23900298f",
+    "url": "/vue-ipo-simulator/js/chunk-vendors.a7a2c5f9.js"
+  },
+  {
+    "revision": "513c50c465d1acc1d6b94d9b41682ced",
+    "url": "/vue-ipo-simulator/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-ipo-simulator/robots.txt"
+  }
+]);
